44e7aa16e9e21264f0416415498f06cab5136958
Getaneh48 <getaneh11@gmail.com>
2024-05-27 06:46:31 +0000

    cors handling added

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 """
+main flask app
 """
 from os import getenv
 from models import storage
 from api.v1.views import app_views
 from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
 
 @app.teardown_appcontext
 def tear_down(self):
@@ -15,16 +19,19 @@ def tear_down(self):
     """
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found(error):
     """
-    return 404 status code response
+    handles the  404 status code response
     """
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
-    # Register the blueprint with the app
+    """
+    main entry
+    """
     app.register_blueprint(app_views, url_prefix='/api/v1')
     app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
             port=int(getenv("HBNB_API_PORT", "5000")), threaded=True)
